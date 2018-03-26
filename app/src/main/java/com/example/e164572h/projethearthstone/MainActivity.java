7e617fef98de26c5b13c0c1248194c74fc2e3d8e
JohnSmoothie <vicoboix@gmail.com>
2018-03-26 12:19:20 +0200

    deuxieme vue

--- app/src/main/java/com/example/e164572h/projethearthstone/MainActivity.java
+++ app/src/main/java/com/example/e164572h/projethearthstone/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.e164572h.projethearthstone;
 
 import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -27,13 +30,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gestionSpinner(View v) {
-        Spinner spinnerClasse = (Spinner) findViewById(R.id.spinnerClasse);
-        Spinner spinnerType = (Spinner) findViewById(R.id.spinnerType);
-        Spinner spinnerFaction = (Spinner) findViewById(R.id.spinnerFaction);
-        Spinner spinnerRace = (Spinner) findViewById(R.id.spinnerRace);
+        final Spinner spinnerClasse = (Spinner) findViewById(R.id.spinnerClasse);
+        final Spinner spinnerType = (Spinner) findViewById(R.id.spinnerType);
+        final Spinner spinnerFaction = (Spinner) findViewById(R.id.spinnerFaction);
+        final Spinner spinnerRace = (Spinner) findViewById(R.id.spinnerRace);
         //exemple adapter
         Integer [] data = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        spinnerClasse.setAdapter(adapter);
+
+        spinnerClasse.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String data = spinnerClasse.getSelectedItem().toString();
+                versVueList(data);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String data = spinnerType.getSelectedItem().toString();
+                versVueList(data);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        spinnerFaction.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String data = spinnerFaction.getSelectedItem().toString();
+                versVueList(data);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        spinnerRace.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String data = spinnerRace.getSelectedItem().toString();
+                versVueList(data);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
     }
 
     public void jsonVolley(View v) {
@@ -65,5 +117,11 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         queue.add(stringRequest);
+    }
+
+    public void versVueList(String data) {
+        Intent versVueList = new Intent(MainActivity.this, ListActivity.class);
+        versVueList.putExtra(Intent.EXTRA_TEXT, data);
+        startActivity(versVueList);
     }
 }
